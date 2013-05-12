41d3615a64ab131eea1ac7d80b2ef3240e6da460
Roy Schroder <r.schroder@live.nl>
2013-05-12 21:23:01 +0200

    push

--- Prototype/Assets/Game/Scripts/LevelLoaderScript.js
+++ Prototype/Assets/Game/Scripts/LevelLoaderScript.js
@@ -1,4 +1,5 @@
 #pragma strict
+public var songArray = new Array();
 
 function Start ()
 {
@@ -10,10 +11,17 @@ function Update ()
 
 function PushCube(soundCode:String)
 {
-
+songArray.Push(soundCode);
+TestArray();
 }
 
 function LoadLevel()
 {
 
+}
+
+function TestArray(){
+for(var code:String in songArray)
+    Debug.Log(code);
+    
 }
