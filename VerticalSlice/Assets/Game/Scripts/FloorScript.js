145b72c718aa3210590cf7dacc37ed0b512037a6
Kevin Mettes <k.mettes@home.nl>
2013-05-28 14:46:07 +0200

    floorscript edited

--- VerticalSlice/Assets/Game/Scripts/FloorScript.js
+++ VerticalSlice/Assets/Game/Scripts/FloorScript.js
@@ -5,26 +5,39 @@ var randomNumber:float = 0; //starts inactive
 
 function Start ()
 {
-	randomize();
+	
 }
 
 function Update ()
 {
-	colour();
+	randomize();
 }
 
 private function randomize()
 {
 	//random number from 0-6
 	randomNumber = Random.Range(0, 6);
+	colour();
 }
 
 private function colour()
 {
-	if(randomNumber == 0) Debug.Log("inactive");
-	if(randomNumber == 1) Debug.Log("blue");
-	if(randomNumber == 2) Debug.Log("green");
-	if(randomNumber == 3) Debug.Log("red");
-	if(randomNumber == 4) Debug.Log("yellow");
-	if(randomNumber == 5) Debug.Log("purple");
+	if(randomNumber == 0) colour("inactive");
+	if(randomNumber == 1) colour("blue");
+	if(randomNumber == 2) colour("green");
+	if(randomNumber == 3) colour("red");
+	if(randomNumber == 4) colour("yellow");
+	if(randomNumber == 5) colour("purple");
+}
+
+private function colour(colour:String)
+{
+	//change shader to colour
+	// Create a material from code
+        // Create a material with transparent diffuse shader
+        var material = new Material (Shader.Find ("Transparent/Diffuse"));
+        material.mainTexture = 
+        // assign the material to the renderer
+        renderer.material = material;
+        Debug.Log("changing colour");
 }
