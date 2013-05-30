1a273ff34c7719cc0bfc84c76183cc40cebc6e9f
Kevin Mettes <k.mettes@home.nl>
2013-05-30 21:27:01 +0200

    Terminate script edited
    countdown feature

--- VerticalSlice/Assets/Game/Scripts/Terminate.js
+++ VerticalSlice/Assets/Game/Scripts/Terminate.js
@@ -1,12 +1,72 @@
 #pragma strict
 
-function Start () {
+var playFirstTime:boolean = true;
+var timer:float = 250;
+var countDown:String;
+var TextStyle:GUIStyle;
+
+function Start ()
+{
 
 }
 
+function Awake()
+{
+	//change the font size to value
+	TextStyle.fontSize = 128;
+	//change the colour of the font
+	TextStyle.normal.textColor = Color.white;
+}
+
+function OnGUI()
+{
+	//countdown display
+	if(playFirstTime == true)
+	{
+		GUI.Label(new Rect(Screen.width / 2, 0, 30, 80), countDown, TextStyle);
+	}
+}
+
+public function playSequence()
+{
+	//set the boolean on false
+	playFirstTime = false;
+	//get the array of cubes	
+	var array:Array = GameObject.Find("LevelLoader").GetComponent(LevelLoaderScript).getCubeList();
+	
+	for (var i:int = 0; i < array.length; i++)
+	{
+		//get the cube from the array
+		var cube:GameObject = array[i];
+		//play the sound of the cube if it contains any
+		cube.GetComponent(SoundCubeScript).playDelayed(i % 8);
+	}
+}
+
+
+
 function Update ()
 {
-
+	//if it has to play the first time
+	if(playFirstTime == true)
+	{
+		//decrease timer by 1
+		timer--;
+		//Countdown show
+		if(timer == 240) countDown = "3";
+		if(timer == 160) countDown = "2";
+		if(timer == 80) countDown = "1";
+		//if time is up
+		if(timer == 0)
+		{
+			//show nothing in the countdown
+			countDown = "";
+			//play the sequence
+			playSequence();
+		}
+	}
+	
+	
 	if(Input.GetKeyUp("f"))
 	{
 		//get current level
@@ -22,14 +82,17 @@ function Update ()
 	
 	if(Input.GetKeyUp("space"))
 	{
+		//get the array with cubes
 		var array:Array = GameObject.Find("LevelLoader").GetComponent(LevelLoaderScript).getCubeList();
-		
 	
-		for (var i:int = 0; i < array.length; i++){
-		var cube:GameObject = array[i];
-		cube.GetComponent(SoundCubeScript).playDelayed(i % 8);
-		//cube.GetComponent(SoundCubeScript).resetDelayed();
-		
+		for (var i:int = 0; i < array.length; i++)
+		{
+			//store the cube in a variable for usage
+			var cube:GameObject = array[i];
+			//play the sound of the cube
+			cube.GetComponent(SoundCubeScript).playDelayed(i % 8);
+			
+			//call increase try function
 		}
 		
 	}
